2cc4d115604d3984a3196b7e120e58bd63ea6593
ahmad-delforouzi <81425722+ahmad-delforouzi@users.noreply.github.com>
2021-04-04 12:16:54 +0200

    Update calc.js

--- static/calc.js
+++ static/calc.js
@@ -92,7 +92,7 @@ function mean1()
             $.ajax(
             {
                     type : 'POST',
-                    url : "/mean",
+                    url : "/mean1",
                     contentType: 'application/json;charset=UTF-8',
                     data : JSON.stringify({"data" : document.getElementById("result").value}),
                     success: function(response){
